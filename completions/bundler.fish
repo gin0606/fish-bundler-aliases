689fb942b5b14d10d41b75530a0d329e3c713fdf
gin0606 <kkgn06@gmail.com>
2016-08-30 17:57:21 +0900

    Add aliases

---
+++ completions/bundler.fish
@@ -0,0 +1,9 @@
+abbr be "bundle exec"
+abbr bl "bundle list"
+abbr bp "bundle package"
+abbr bo "bundle open"
+abbr bout "bundle outdated"
+abbr bu "bundle update"
+abbr bi bundle install --jobs=(sysctl -n hw.ncpu)
+abbr bip bundle install --jobs=(sysctl -n hw.ncpu) --path .bundle
+abbr bcn "bundle clean"
